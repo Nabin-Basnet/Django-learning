4927f0233259c65fc91df8bcdfcba745136bbb5e
nabin1200 <nabinbasnet1200@gmail.com>
2025-03-17 11:35:59 +0545

    install pillow for image

--- myapp/models.py
+++ myapp/models.py
@@ -5,4 +5,5 @@ class person(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
     address=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='media',null=True,blank=True)
 
