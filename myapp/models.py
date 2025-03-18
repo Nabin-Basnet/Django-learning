b8a875c47485a63ca2160485c4c56078cd6e5618
nabin1200 <nabinbasnet1200@gmail.com>
2025-03-18 12:55:32 +0545

    data fatching

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,6 @@ class person(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField(null=True)
     address=models.CharField(max_length=50,null=True)
-    image=models.ImageField(upload_to='media',null=True,blank=True)
+    image=models.ImageField(upload_to='media/',null=True,blank=True)
     discription=models.TextField(null=True)
 
--- myapp/views.py
+++ myapp/views.py
@@ -4,15 +4,19 @@ from .models import person
 # Create your views here.
 def home(request):
     if request.method=='POST':
-        name=request.POST["name"]     
-        p=person.objects.create(name=name)
+        name=request.POST["name"]
+        age=request.POST["age"]   
+        address=request.POST["address"]   
+        discription=request.POST["discription"]
+        image=request.FILES["image"]  
+        p=person.objects.create(name=name,age=age,address=address,discription=discription,image=image)
         p.save()
         return redirect('home')
     return render (request,"index.html")
 
-def index(request):
-    p=person.objects.all()
-    return render(request,"index.html",{"p":p})
-
 def about(request):
-    return render(request,"about.html")
+    p=person.objects.all()
+    return render(request,"about.html",{'p':p})
+
+# def about(request):
+#     return render(request,"about.html")
